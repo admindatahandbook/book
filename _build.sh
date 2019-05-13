f8b3b11624906271f241fa18832024cfae9905cc
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-05-13 16:49:02 -0400

    Initial mods

--- _build.sh
+++ _build.sh
@@ -5,7 +5,7 @@ set -ev
 cd $(dirname $0)
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "rmarkdown::render('LICENSE.Rmd', 'html_document', '_book/license.html')"
+#Rscript -e "rmarkdown::render('LICENSE.Rmd', 'html_document', '_book/license.html')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
