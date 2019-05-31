45decfa737496cf10572eea8c1329b06a0526e3d
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-05-31 07:35:48 -0600

    Adding R files for local testing

---
+++ test.R
@@ -0,0 +1,5 @@
+# This runs the book render locally. It is only used for testing.
+
+bookdown::render_book('index.Rmd', 'bookdown::gitbook')
+#bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
+
