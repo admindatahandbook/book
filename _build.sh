ada0961f636d2212f88ac3fe94ac82fdbe6c72ef
jimhshen <jim.h.shen@gmail.com>
2020-04-01 16:05:54 -0400

    test

--- _build.sh
+++ _build.sh
@@ -4,7 +4,6 @@ set -ev
 
 cd $(dirname $0)
 
-Rscript -e 'install.packages("ggplot2")' -e 'remotes::install_deps(".", dependencies = TRUE)'
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "rmarkdown::render('LICENSE.Rmd', 'html_document', '_book/license.html')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
