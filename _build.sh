270b39219aeaeeffd62e3c2472f1c9f38f6425e4
jimhshen <jim.h.shen@gmail.com>
2020-04-04 23:07:17 -0400

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -10,7 +10,3 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "rmarkdown::render('LICENSE.Rmd', 'html_document', '_book/license.html')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
-
-
-# move all images over
-mv _main_files _book
