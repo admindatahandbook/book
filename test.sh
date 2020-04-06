1b66f399e49dbd450968b42ef01fbbf05a9a6fd4
Lars Vilhuber <lars@vilhuber.com>
2020-04-05 21:38:04 -0400

    Small changes

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-docker run  --rm -v $(pwd):/documents conoria/alpine-r-bookdown  /documents/_build.sh
+#docker run  --rm -v $(pwd):/documents conoria/alpine-r-bookdown  /documents/_build.sh
+docker run  --rm -v $(pwd):/documents --name bookdown rocker/r-rmd  /documents/_build.sh
+
