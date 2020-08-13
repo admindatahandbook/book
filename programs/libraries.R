857d8dfcb6251ae5bbd34a82ac4bad538975b85c
jimhshen <jim.h.shen@gmail.com>
2020-08-13 18:25:10 -0400

    trying out atusydown

--- programs/libraries.R
+++ programs/libraries.R
@@ -6,35 +6,35 @@
 # Will install all necessary libraries
 #
 
-#pkgTest <- function(x)
-#{
-#	if (!require(x,character.only = TRUE))
-#	{
-#		print(paste0("Package ",x," not found - installing"))
-#		install.packages(x,repos = 'https://cloud.r-project.org/')
-#		if(!require(x,character.only = TRUE)) stop("Package not found")
-#	}
-#	return("OK")
-#}
-
-#global.libraries <- c("here","bookdown","tufte","config","ggplot2","rprojroot","memisc","forcats","kableExtra")
-
-#results <- sapply(as.list(global.libraries), pkgTest)
-
-
-
-args = commandArgs(trailingOnly=TRUE)
-
-if (length(args)>0) {
-  if ( args[1] == "ubuntu" ) {
-    system("apt-get update")
-    system("apt-get upgrade")
-    system("apt-get install -y libxml2-dev")
-    system("apt-get install -y pandoc-citeproc")
-    system("apt-get install -y r-cran-rprojroot r-cran-tidyverse")
-    system("apt-get install -y r-cran-here r-cran-bookdown")
-    system("apt-get install -y r-cran-tufte r-cran-config r-cran-memisc")
-    system("apt-get install -y r-cran-forcats")
-    system("apt-get install -y r-cran-kableExtra ")
-  }
+pkgTest <- function(x)
+{
+	if (!require(x,character.only = TRUE))
+	{
+		print(paste0("Package ",x," not found - installing"))
+		install.packages(x,repos = 'https://cloud.r-project.org/')
+		if(!require(x,character.only = TRUE)) stop("Package not found")
+	}
+	return("OK")
 }
+
+global.libraries <- c("here","bookdown","tufte","config","ggplot2","rprojroot","memisc","forcats","kableExtra")
+
+results <- sapply(as.list(global.libraries), pkgTest)
+
+
+
+#args = commandArgs(trailingOnly=TRUE)
+#
+#if (length(args)>0) {
+#  if ( args[1] == "ubuntu" ) {
+#    system("apt-get update")
+#    system("apt-get upgrade")
+#    system("apt-get install -y libxml2-dev")
+#    system("apt-get install -y pandoc-citeproc")
+#    system("apt-get install -y r-cran-rprojroot r-cran-tidyverse")
+#    system("apt-get install -y r-cran-here r-cran-bookdown")
+#    system("apt-get install -y r-cran-tufte r-cran-config r-cran-memisc")
+#    system("apt-get install -y r-cran-forcats")
+#    system("apt-get install -y r-cran-kableExtra ")
+#  }
+#}
