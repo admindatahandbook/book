6369910ac0458095126064f49672fdb237ba0710
jimhshen <jim.h.shen@gmail.com>
2020-08-18 12:45:29 -0400

    Update libraries.R

--- programs/libraries.R
+++ programs/libraries.R
@@ -17,6 +17,6 @@ pkgTest <- function(x)
 	return("OK")
 }
 
-global.libraries <- c("tufte","config","ggplot2","rprojroot","memisc","forcats","kableExtra","colorBrewer")
+global.libraries <- c("tufte","config","ggplot2","rprojroot","memisc","forcats","kableExtra","RColorBrewer")
 
 results <- sapply(as.list(global.libraries), pkgTest)
