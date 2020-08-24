971fb29eb000c13190832e3e26d8119bc4d8dc9c
jimhshen <jim.h.shen@gmail.com>
2020-08-24 10:14:04 -0400

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -4,7 +4,7 @@ set -ev
 
 cd $(dirname $0)
 
-Rscript -e "options(bookdown.render.file_scope = FALSE); bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "rmarkdown::render('LICENSE.Rmd', 'html_document', '_book/license.html')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
