118beba77547ba54e3a29f5766501f5f5e4bea2e
jimhshen <jim.h.shen@gmail.com>
2020-08-26 10:57:55 -0400

    can we install pander

--- programs/libraries.R
+++ programs/libraries.R
@@ -17,7 +17,7 @@ pkgTest <- function(x)
 	return("OK")
 }
 
-global.libraries <- c("here","knitr","bookdown","tufte","config","ggplot2","rprojroot","memisc","forcats","kableExtra","RColorBrewer","distill")
+global.libraries <- c("here","knitr","bookdown","tufte","config","ggplot2","rprojroot","memisc","forcats","kableExtra","RColorBrewer","distill","pander")
 
 results <- sapply(as.list(global.libraries), pkgTest)
 
