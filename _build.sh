9eca11527c41115b175f5d19f88d265b0cfcb6dc
Lars Vilhuber <lars@vilhuber.com>
2020-09-21 15:35:55 -0400

    Adding some tools

--- _build.sh
+++ _build.sh
@@ -4,9 +4,16 @@ set -ev
 
 cd $(dirname $0)
 
+# set up the environment
 Rscript _bootstrap.R
+
+# build the handbook
 Rscript _build.R
-#Rscript -e "options(bookdown.render.file_scope = FALSE); bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-#Rscript -e "rmarkdown::render('LICENSE.Rmd', 'html_document', '_book/license.html')"
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
+# move over a few files
+if [[ -d _book ]] 
+then
+  mkdir _book/_process
+  [[ -f _process/index.tmpl ]] && cp _process/index.tmpl _book/_process
+  [[ -f _process/set_version.sh ]] && cp _process/set_version.sh _book/_process
+fi
