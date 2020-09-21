b21cd3c0259cb99bb32cbae830207e11c8b96344
jimhshen <jim.h.shen@gmail.com>
2020-09-21 01:34:30 -0400

    add introduction chapter

---
+++ programs/_plot_intro.R
@@ -0,0 +1,18 @@
+# Author: Jim Shen
+# Description: This will generate various plots for the intro chapter.
+
+introt1data<-read.csv("./assets/intro/introtable1.csv")
+
+introtable1<-function(){
+  knitr::kable(introt1data, booktabs = TRUE,
+               caption = "Descriptive Table of Chapters",
+               col.names = c("Name of Institution",
+                             "Type of Data Provider",
+                             "Data Intermediary/Data Holder",
+                             "Highlight")) %>%
+    kable_styling(full_width = F) %>%
+    column_spec(1, width="10em") %>%
+    column_spec(2, width="10em") %>%
+    column_spec(3, width="20em") %>%
+    column_spec(4, width="20em")
+}
