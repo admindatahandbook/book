5432618602fb30ca803a4e4b5573109b41b38ba8
Lars Vilhuber <lars.vilhuber@cornell.edu>
2020-09-24 15:49:26 -0400

    Re #5 fixes the issues with leading/trailing characters (also minor edits to IAB as a consequence) (#48)

--- programs/filter-index-html.lua
+++ programs/filter-index-html.lua
@@ -6,12 +6,13 @@ end
 return {
   {
     Str = function (elem)
-      a,b,text = string.find(elem.text,"(%b+|)")
+      a,b,before,text,after = string.find(elem.text,"(.*)(%b+|)(.*)")
       if isempty(a) then
         -- we did not find a pattern
         return elem
       else
-        elem.text = string.gsub(string.sub(text,2,-2),"_"," ")
+        -- rebuild elem
+        elem.text = before .. string.gsub(string.sub(text,2,-2),"_"," ") .. after
         return  elem 
       end
     end,
