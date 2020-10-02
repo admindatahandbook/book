2fa2ef7c42187da6e5ecf5b34dbe420a0a738f24
jimhshen <jim.h.shen@gmail.com>
2020-10-02 17:11:41 -0400

    Issue54 (#69)
    * rest of issue 54
    
    * Update ideahandbook.bib

--- programs/_plot_iab.R
+++ programs/_plot_iab.R
@@ -73,14 +73,14 @@ iabt1data <- data.frame(
   Features = c(
     "2 percent random sample (more than 1.8 million individuals) of the Integrated Employment Biographies, a database which includes records from:<br>- 1975 onwards: employment subject, receipt of benefits in accordance with Social Code Act III<br>- 1999 onwards: Marginal part-time employment<br>- 2000 onwards: participation in an employment or training measure<br>- 2007 onwards: Registered jobseeker",
     "1975 until 2017 for West Germany, 1992 until 2017 for East Germany, updated every 2nd year by RDC-IAB", 
-    "DOI: 10.5164/IAB.SIAB7517.de.en.v1",
+    "DOI: [10.5164/IAB.SIAB7517.de.en.v1](https://doi.org/10.5164/IAB.SIAB7517.de.en.v1)",
     "Repeated cross-sectional dataset (640,000-1.5 million establishments) on June 30, on all establishments with at least one employee liable to social security (until 1988) and/or at least one marginal worker (since 1999), and thereof a 50percent random sample",
     "1975 until 2018 for West Germany, 1992 until 2018 for East Germany, annual updated by RDC-IAB",
-    "DOI: 10.5164/IAB.BHP7518.de.en.v1",
-    "Worker information from administrative data are linked to the IAB-Establishment Panel, an annual representative survey since 1993 in West Germany and 1996 in East Germany including approx. 16,000 establishments per year
+    "DOI: [10.5164/IAB.BHP7518.de.en.v1](https://doi.org/10.5164/IAB.BHP7518.de.en.v1)",
+    "Worker information from administrative data are linked to the IAB-Establishment Panel, an annual representative survey since 1993 in West Germany and 1996 in East Germany including approx. 16,000 establishments per year<br>
     Stratified sample of all establishments with at least one employee liable to social security on June 30 of the year before by establishment size, industry, and federal state. There are two versions of LIAB:<br>- LIAB cross-sectional model: all establishment surveys combined with administrative worker data as of June 30 (establishment: 67,407 in total; individuals approx. 12.5 million in total)<br>- LIAB longitudinal model: selection of repeatedly interviewed establishments (2009-2016) combined with longitudinal worker biographies (establishment: 41,777 in total; individuals approx. 1.7 million in total)",
     "LIAB Cross-sectional model: 1993-2017<br>LIAB longitudinal model: 1975-2017",
-    "DOI: 10.5164/IAB.LIABQM29317.de.en.v1<br>DOI: 10.5164/IAB.LIABLM7517.de.en.v1"
+    "DOI: [10.5164/IAB.LIABQM29317.de.en.v1](https://doi.org/10.5164/IAB.LIABQM29317.de.en.v1)<br>[DOI: 10.5164/IAB.LIABLM7517.de.en.v1](https://doi.org/10.5164/IAB.LIABLM7517.de.en.v1)"
   )
 )
 
@@ -88,7 +88,7 @@ iabtable1 <- function(df){
   if (is_html_output()) {
     df %>%
       kable("html", escape = F, caption="Selected RDC-IAB data", col.names = NULL, booktabs=T) %>%
-      kable_styling(full_width = F) %>%
+      kable_styling(full_width = F, font_size=12) %>%
       column_spec(1, width="10em") %>%
       #column_spec(2, width="25em") %>%
       pack_rows("Sample of Integrated Labour Market Biographies (SIAB)",1,3) %>%
@@ -128,5 +128,5 @@ iabt2data<-read.csv("./assets/iab/table2.csv")
 iabtable2<-function(){
   knitr::kable(iabt2data, booktabs = TRUE,
                caption = "Number of publications in 2018, including all publications with RDC-IAB data (excluding bachelor and master theses)") %>%
-    kable_styling(full_width = F)
+    kable_styling(full_width = F, font_size=12)
 }
