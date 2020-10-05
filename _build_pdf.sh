c86aa512627a42b772c4a7df1d0c3f4cd34f9c87
Lars Vilhuber <lars.vilhuber@cornell.edu>
2020-10-04 22:28:39 -0400

    Re #73 further updates
    - now with chapterbib - requires changes to all Rmd files
    - indexing still turned off - but text cleaned

--- _build_pdf.sh
+++ _build_pdf.sh
@@ -22,13 +22,24 @@ Rscript programs/build_pdf.R > _R.log 2>&1
 sed -i 's/NULL//' _main.tex
 sed -i 's/\\chapter\*{(PART) /\\part*{/' _main.tex
 sed -i 's/\\chapter\*{(APPENDIX) /\\part*{/' _main.tex
+#sed -i 's/^\\chapter{/\putbib\n\\chapter{/' _main.tex 
 sed -i 's/\\addcontentsline{toc}{chapter}{(PART) /\\partline{/' _main.tex
 sed -i 's/\\addcontentsline{toc}{chapter}{(APPENDIX) /\\partline{/' _main.tex
 
 # now compile it
 
 $LATEX ${FILE}.tex > _tex.log 2>&1
-bibtex ${FILE}     > _bib.log 2>&1 || echo "Completed bibtex with an error"
+#bibtex ${FILE}     > _bib.log 2>&1 || echo "Completed bibtex with an error"
+[ -f _bib.log ] && rm _bib.log
+for arg in 21 22 23 24
+do
+  [ -f bu${arg}.aux ] && rm bu${arg}.aux
+done
+for arg in $(ls bu*aux)
+do
+	bibtex $arg >> _bib.log 2>&1 || echo "Completed bibtex with an error"
+done
+$LATEX ${FILE}.tex > _tex.log 2>&1
 $LATEX ${FILE}.tex > _tex.log 2>&1
 $LATEX ${FILE}.tex > _tex.log 2>&1
 
