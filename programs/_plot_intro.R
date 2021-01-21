e25596be9130df9cdd21844257a382c95bd036a5
jimhshen <jim.h.shen@gmail.com>
2021-01-21 15:27:55 -0500

    stop building unused intro graph

--- programs/_plot_intro.R
+++ programs/_plot_intro.R
@@ -76,23 +76,23 @@ introtable1<-function(){
 
 # Source: Chetty (2012) via https://github.com/larsvilhuber/clone-chetty-use-admin-data
 
-cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
-#raw_url <- "https://raw.githubusercontent.com/larsvilhuber/clone-chetty-use-admin-data/master/chetty1_increase_admin.csv"
-#chetty <- read.csv(raw_url,header=TRUE)
-chetty <- read.csv("./assets/intro/chetty1_increase_admin.csv",header=TRUE)
-chetty2 <- gather(chetty,Journal,adminpct,AER,JPE,QJE,ECMA,-Year)
-intrograph1 <- ggplot(chetty2,aes(Year,adminpct,color=Journal)) + 
-  geom_line()  + 
-  theme_minimal() +
-  theme(panel.grid.minor = element_blank()) +
-  scale_colour_manual(values=cbbPalette) +
-  ylab("") 
-ggsave("./figures/intrograph1.png",intrograph1,dpi=300)
-pdffile <- "./figures/intrograph1.pdf"
-if ( ! file.exists(pdffile) ) {
-  ggsave(pdffile,intrograph1,dpi=pdfdpi)
-}
+# cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+#
+# #raw_url <- "https://raw.githubusercontent.com/larsvilhuber/clone-chetty-use-admin-data/master/chetty1_increase_admin.csv"
+# #chetty <- read.csv(raw_url,header=TRUE)
+# chetty <- read.csv("./assets/intro/chetty1_increase_admin.csv",header=TRUE)
+# chetty2 <- gather(chetty,Journal,adminpct,AER,JPE,QJE,ECMA,-Year)
+# intrograph1 <- ggplot(chetty2,aes(Year,adminpct,color=Journal)) +
+#   geom_line()  +
+#   theme_minimal() +
+#   theme(panel.grid.minor = element_blank()) +
+#   scale_colour_manual(values=cbbPalette) +
+#   ylab("")
+# ggsave("./figures/intrograph1.png",intrograph1,dpi=300)
+# pdffile <- "./figures/intrograph1.pdf"
+# if ( ! file.exists(pdffile) ) {
+#   ggsave(pdffile,intrograph1,dpi=pdfdpi)
+# }
 
 
 
