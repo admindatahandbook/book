cf0ccc39079d87d57d0c1d3c4d05a462dc8ffc60
Lars Vilhuber <lars@vilhuber.com>
2021-02-14 21:56:07 -0500

    Re #196 addressing the PART thing

--- _build_ebook.sh
+++ _build_ebook.sh
@@ -26,12 +26,27 @@ cp _main.epub handbook.epub
 sed -i 's+grouplength="3"++' EPUB/text/ch005.xhtml 
 sed -i 's+indentlevel="1"++' EPUB/text/ch005.xhtml 
 
+# Part
+for arg in 06 12 
+do
+  sed -i 's+section id="part+section epub:type="part" id="part+' EPUB/text/ch0$arg.xhtml
+  sed -i 's+(PART) ++g' EPUB/text/ch0$arg.xhtml
+done
+for arg in nav.xhtml toc.ncx
+do
+  sed -i 's+(PART) ++g' EPUB/$arg
+done
+
+
 for arg in 10 11 13 14 16 
 do
   sed -i 's+colspan="100%"+colspan="10"+' EPUB/text/ch0$arg.xhtml 
   sed -i 's+<br>+<br/>+g' EPUB/text/ch0$arg.xhtml 
 done
-zip -rp handbook.epub EPUB/text/ch*
+
+# package it all up again
+
+zip -rp handbook.epub EPUB/text/ch* EPUB/nav.xhtml EPUB/toc.ncx
 
 # next steps
 echo "Using calibre, convert EPUB to MOBI and AZW3"
