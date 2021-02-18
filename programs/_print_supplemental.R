be3ac95d7f446ad1a8e760f11d4df8180cb7f55f
jimhshen <jim.h.shen@gmail.com>
2021-02-18 15:25:45 -0500

    update print supplemental script, DIME missing link

--- programs/_print_supplemental.R
+++ programs/_print_supplemental.R
@@ -6,17 +6,35 @@
 printsupp<-function(chapter,debug=FALSE){
   webinar <- config::get(chapter)$webinar
   slides <- config::get(chapter)$slides
+  webinardate <- config::get(chapter)$webinardate
+  authorlist <- config::get(chapter)$authors
   
-  if (length(webinar)!=0 | length(slides)!=0) {
-    cat("## Supplemental Materials {-} \n")
+  if (length(authorlist)==1) {
+    if (length(webinar)!=0) {
+      cat("## Supplemental Materials {-} \n")
+    }
+    if (length(webinar)!=0 & length(slides)!=0) {
+      cat(paste0("This chapter was presented by the author as a webinar on ",webinardate,". A [recording of the webinar](",webinar,") and [slides](",slides,") are available. \n"))
+    }
+    if (length(webinar)!=0 & length(slides)==0) {
+      cat(paste0("This chapter was presented by the author as a webinar on ",webinardate,". A [recording of the webinar](",webinar,") is available. \n"))
+    }
+    if (length(webinar)!=0 | length(slides)!=0) {
+      cat(" \n")
+    }
   }
-  if (length(webinar)!=0) {
-    cat(paste0("The video of this chapter's webinar presentation can be found [here](",webinar,"). \n"))
-  }
-  if (length(slides)!=0) {
-    cat(paste0("The slides for the webinar presentation are available [here](",slides,"). \n"))
-  }
-  if (length(webinar)!=0 | length(slides)!=0) {
-    cat(" \n")
+  if (length(authorlist)>1) {
+    if (length(webinar)!=0) {
+      cat("## Supplemental Materials {-} \n")
+    }
+    if (length(webinar)!=0 & length(slides)!=0) {
+      cat(paste0("This chapter was presented by the authors as a webinar on ",webinardate,". A [recording of the webinar](",webinar,") and [slides](",slides,") are available. \n"))
+    }
+    if (length(webinar)!=0 & length(slides)==0) {
+      cat(paste0("This chapter was presented by the authors as a webinar on ",webinardate,". A [recording of the webinar](",webinar,") is available. \n"))
+    }
+    if (length(webinar)!=0 | length(slides)!=0) {
+      cat(" \n")
+    }
   }
 }
